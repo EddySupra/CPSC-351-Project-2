3578f442dec6d5d9ec8eaada491726c6354ed56d
EddySupra <eduardo23@csu.fullerton.edu>
2023-03-10 20:33:23 -0800

    done

--- time_shm.c
+++ time_shm.c
@@ -11,31 +11,31 @@
 
 // Group 1
 // Tak Cheng, Muhammad Aslam, Eduardo Casas-Gabirio
-void get_input(char last_command[], char* parameter[]) 
+void get_input(char last_command[], char* parameter[])
 {
     //create an array to store input
-    char input[1024] = {0}; 
-    
+    char input[1024] = {0};
+
     //initialize variables and pointers
     int a = 0, i = 0, j = 0;
     char* arr[100], *mult_input_str;
-    
+
     // while loop tp read input from user
     while (1)
     {
         int c = fgetc(stdin);//fgetc returns next character as int
         input[a++] = (char)c;// store all characters in input array as char.
-        
+
         //Exit while loop once user presses enter
-        if (c == '\n') 
+        if (c == '\n')
         {
-            break; 
+            break;
         }
     }
     if (a == 1) return;
 
     mult_input_str = strtok(input, " \n");// Split our input array into tokens using strtok
-    while (mult_input_str != NULL) 
+    while (mult_input_str != NULL)
     {
         arr[i++] = strdup(mult_input_str);// use strdup to duplicate string command arr
         mult_input_str = strtok(NULL, " \n");// split after newline
@@ -43,14 +43,14 @@ void get_input(char last_command[], char* parameter[])
 
     // command that will be passed to main
     strcpy(last_command, arr[0]);
-    
-    // copy and prepare parameter to be used when executing command 
+
+    // copy and prepare parameter to be used when executing command
     for (int j = 0; j < i; j++)
     {
         parameter[j] = arr[j];
         parameter[i] = NULL;
     }
-    
+
     // exit program if user command = exit
     if (strcmp(last_command, "exit") == 0) //check if strings are equal using strcmp
     {
@@ -62,13 +62,13 @@ void get_input(char last_command[], char* parameter[])
     {
         system("bash -c help");
     }
-    else 
+    else
     {
         printf("\n");
     }
 }
 
-int main(int argc, const char** argv) 
+int main(int argc, char** argv)
 {
     int status;
     // arrays to store the commands as well as multiple parameters
@@ -76,37 +76,37 @@ int main(int argc, const char** argv)
     // path, env variable array of pointers
     char *env[] = { (char*) "PATH=/bin", 0 };
     int p[2];
-    
-    //assign shared memory pointer contents to struct 
+
+    //assign shared memory pointer contents to struct
     struct timeval *time_ptr = (struct timeval *)malloc(sizeof(struct timeval));
     struct timeval start_time, end_time, elapsed_time;
     const char *shared_memory_name = "Shared memory segment";
-    
+
     //establish a shared memory object with shm_open
     int shared_memory;
     shared_memory = shm_open(shared_memory_name, O_CREAT | O_RDWR, 0666);
-    
+
     //make shared_memory size of struct timeval
     ftruncate(shared_memory, sizeof(struct timeval));
-    
+
     //
     time_ptr = (struct timeval *)mmap(0, sizeof(struct timeval), PROT_READ | PROT_WRITE, MAP_SHARED, shared_memory, 0);
     close(shared_memory);
-    
-    //Welcome user 
-    printf("Welcome to myShell\n");
+
+    //Welcome user
+    //printf("Welcome to myShell\n");
     // while loop till user chooses to exit
     int i = 0;
-    while (i != 1) 
+    while (i != 1)
     {
-        
-        printf("==>");
-        
+
+        //printf("==>");
+
         // Get user input.
         get_input(user_input, user_input_parameters);
-        
+
         // parent process.
-        if (fork() != 0) 
+        if (fork() != 0)
         {
             //parent process
             wait(&status);
@@ -121,16 +121,16 @@ int main(int argc, const char** argv)
             i = 1;
         }
         //child process
-        else 
+        else
         {
-        //get current time 
+        //get current time
         struct timeval start_time;
         gettimeofday(&start_time, 0);
-        
+
         //copy sizeof(struct timeval) from start_time to time_ptr
         memcpy(time_ptr, &start_time, sizeof(struct timeval));
         execvp(argv[1], &argv[1]);
-        
+
         //exit child process
         exit(0);
         }
